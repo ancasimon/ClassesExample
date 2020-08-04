1719b99bd8d4548f34598e4918587a9f217af095
Anca Simon <ancarsimon@gmail.com>
2020-08-04 15:17:12 -0500

    Add project files.

---
+++ ClassesExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassesExample.Animals;
+using System;
+//System is a namespace that Microsoft built in .NET 
+
+//namespaces follow file structure - for example code about animals would go in: namespace ClassesExample.Animals
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken = new Chicken("Liza");
+            var bob = new Chicken("Bob");
+
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+
+            bob.Peck("watermelon");
+
+            Console.WriteLine($"{chicken.Name} and {bob.Name} are chickens.");
+        }
+    }
+
+    class Steve
+    {
+
+    }
+}
